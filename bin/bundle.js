c961296307768533dcdb0e4e0e8fc9788d383546
dengzhifeng <398577351@qq.com>
2021-02-03 17:43:15 +0800

    update:  命令行添加

---
+++ bin/bundle.js
@@ -0,0 +1,15 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
+
+	/*
+	 * @description:
+	 * @author: steve.deng
+	 * @Date: 2021-02-03 16:57:56
+	 * @LastEditors: steve.deng
+	 * @LastEditTime: 2021-02-03 17:39:43
+	 */
+	console.log('xxxx11');
+
+})));
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@
  * @author: steve.deng
  * @Date: 2021-02-03 16:57:56
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-02-03 16:58:03
+ * @LastEditTime: 2021-02-03 17:39:43
  */
+console.log('xxxx11');
