962bc11df430824121abdaf8d5bee2001fcb6964
邓志锋 <398577351@qq.com>
2021-03-11 08:10:00 +0800

    update: fix配置

--- rollup.config.js
+++ rollup.config.js
@@ -3,25 +3,29 @@
  * @author: steve.deng
  * @Date: 2021-02-03 17:04:31
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-02-03 17:33:22
+ * @LastEditTime: 2021-03-11 07:44:22
  */
 import path from 'path';
 import ts from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'src/index.ts',
     output: {
         export: 'auto', // 它根据entry模块导出的内容猜测你的意图 https://www.rollupjs.com/guide/big-list-of-options
-        format: 'umd', // umd格式
+        format: 'cjs', // umd格式  //cjs commonjs
         file: path.resolve('bin/bundle.js') // 输出路径 根目录下的bin
     },
     plugins: [
+        // ts编译插件
         ts({
             tsconfig: path.resolve('tsconfig.json')
         }),
+        // node模块路径解析 可以使用node_module里面的模块
         nodeResolve({
             extensions: ['.js', '.ts']
-        })
+        }),
+        commonjs() // commonjs模块转成es6才能 import xx from xx 🍣 A Rollup plugin to convert CommonJS modules to ES6, so they can be included in a Rollup bundle
     ]
 };
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@
  * @author: steve.deng
  * @Date: 2021-02-03 16:57:56
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-03-10 18:13:36
+ * @LastEditTime: 2021-03-11 07:43:29
  */
 console.log('xxxx11');
 import { Command } from 'commander';
 const program = new Command();
+let b = 2;
+console.log(b);
+let text = 0;
+const c = () => {
+    return 2;
+};
+text = c();
+console.log(text);
