d703f7a5b4b77f8566da262de38fef44e2e3b4c5
dengzhifeng <398577351@qq.com>
2021-03-11 18:22:52 +0800

    update: 处理配置

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@
  * @author: steve.deng
  * @Date: 2021-02-03 16:57:56
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-03-11 07:43:29
+ * @LastEditTime: 2021-03-11 18:22:39
  */
-console.log('xxxx11');
-import { Command } from 'commander';
-const program = new Command();
+import program from 'commander';
+import cp from 'child_process';
+import util from 'util';
+import path from 'path';
+import config from './config';
+import { version } from '../package.json';
+const exex = util.promisify(cp.exec);
+
+const defaultConfig = {};
+const usageList = [];
+program.version(version).description(`这是一个命令行工具 版本v${version}`);
+Object.entries(config).forEach(([key, value]) => {});
+
+// 解析传入参数
+program.parse(process.argv);
+
 let b = 2;
 console.log(b);
-let text = 0;
-const c = () => {
-    return 2;
-};
-text = c();
-console.log(text);
