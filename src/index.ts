c46e24e0eb571d2180cbab5d43cf503834fddfb1
dengzhifeng <398577351@qq.com>
2021-03-24 12:02:16 +0800

    feat:2021-03-24-12:01:56

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @author: steve.deng
  * @Date: 2021-02-03 16:57:56
  * @LastEditors: steve.deng
- * @LastEditTime: 2021-03-24 11:48:38
+ * @LastEditTime: 2021-03-24 11:57:29
  */
 import program from 'commander';
 import config from './config';
